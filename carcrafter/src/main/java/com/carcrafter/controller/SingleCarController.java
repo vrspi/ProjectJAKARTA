590a6c953b7f337cb53ed8143f5f4a0a33571571
vrspi <53494644+vrspi@users.noreply.github.com>
2023-12-28 23:53:43 +0000

    Qdd Controller Single Listing

---
+++ carcrafter/src/main/java/com/carcrafter/controller/SingleCarController.java
@@ -0,0 +1,25 @@
+package com.carcrafter.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.carcrafter.model.JPAUtil;
+import com.carcrafter.model.Listing;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/SingleCar")
+public class SingleCarController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+        request.getRequestDispatcher("AddListing.jsp").forward(request, response);
+    }
+}
