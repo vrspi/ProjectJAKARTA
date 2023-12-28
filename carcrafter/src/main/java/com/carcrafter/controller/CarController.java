b0019c92bb6c6463eba7cf29ae86cd204f48e7be
vrspi <53494644+vrspi@users.noreply.github.com>
2023-12-28 23:43:04 +0000

    UI Add Listing

--- carcrafter/src/main/java/com/carcrafter/controller/CarController.java
+++ carcrafter/src/main/java/com/carcrafter/controller/CarController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import com.carcrafter.model.JPAUtil;
-import com.carcrafter.model.Listing;
+import com.carcrafter.model.*;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
@@ -20,16 +20,54 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CarController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
         EntityManager em = JPAUtil.getEntityManager();
 
-        TypedQuery<Listing> query = em.createQuery("SELECT l FROM Listing l", Listing.class);
-        List<Listing> listings = query.getResultList();
+        TypedQuery<BodyType> queryBodyType = em.createQuery("SELECT b FROM BodyType b", BodyType.class);
+        List<BodyType> bodyTypes = queryBodyType.getResultList();
 
-        for (Listing listing : listings) {
-            System.out.println("Listing Title: " + listing.getTitle());
-        }
+        TypedQuery<MakeBrand> queryMakeBrand = em.createQuery("SELECT m FROM MakeBrand m", MakeBrand.class);
+        List<MakeBrand> makeBrands = queryMakeBrand.getResultList();
+
+        TypedQuery<Model> queryModel = em.createQuery("SELECT m FROM Model m", Model.class);
+        List<Model> models = queryModel.getResultList();
+
+        TypedQuery<Year> queryYear = em.createQuery("SELECT y FROM Year y", Year.class);
+        List<Year> years = queryYear.getResultList();
+
+        TypedQuery<DriveType> queryDriveType = em.createQuery("SELECT d FROM DriveType d", DriveType.class);
+        List<DriveType> driveTypes = queryDriveType.getResultList();
+
+        TypedQuery<Transmission> queryTransmission = em.createQuery("SELECT t FROM Transmission t", Transmission.class);
+        List<Transmission> transmissions = queryTransmission.getResultList();
+
+        TypedQuery<FuelType> queryFuelType = em.createQuery("SELECT f FROM FuelType f", FuelType.class);
+        List<FuelType> fuelTypes = queryFuelType.getResultList();
+
+        TypedQuery<Color> queryColor = em.createQuery("SELECT c FROM Color c", Color.class);
+        List<Color> colors = queryColor.getResultList();
+
+        TypedQuery<Doors> queryDoors = em.createQuery("SELECT d FROM Doors d", Doors.class);
+        List<Doors> doorsList = queryDoors.getResultList();
+
+        request.setAttribute("bodyTypes", bodyTypes);
+        request.setAttribute("makeBrands", makeBrands);
+        request.setAttribute("models", models);
+        request.setAttribute("years", years);
+        request.setAttribute("driveTypes", driveTypes);
+        request.setAttribute("transmissions", transmissions);
+        request.setAttribute("fuelTypes", fuelTypes);
+        request.setAttribute("colors", colors);
+        request.setAttribute("doorsList", doorsList);
 
         em.close();
         request.getRequestDispatcher("AddListing.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+
 }
--- carcrafter/src/main/webapp/AddListing.jsp
+++ carcrafter/src/main/webapp/AddListing.jsp
@@ -15,6 +15,7 @@
 
 
     <!-- testimonial area -->
-    <%@ include file="partials/addlisting.jsp" %>
+    <jsp:include page="partials/addlisting.jsp" />
+
     <!-- testimonial area end -->
 </mt:TemplateLoggedin>
