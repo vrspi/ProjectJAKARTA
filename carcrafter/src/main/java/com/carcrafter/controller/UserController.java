182586119fa996c04298b0de9a07eb1c16e7e8e5
HASHAS Jad el mawla <jouj969@gmail.com>
2024-01-14 20:58:11 +0100

    HASHAS
    Correction

--- carcrafter/src/main/java/com/carcrafter/controller/UserController.java
+++ carcrafter/src/main/java/com/carcrafter/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.carcrafter.controller;
 
-import Factory.UserServiceFactory;
+import com.carcrafter.Factory.UserServiceFactory;
 import com.carcrafter.service.UserService;
-import jakarta.persistence.TypedQuery;
 import jakarta.servlet.ServletException;
 
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.*;
-import com.carcrafter.model.JPAUtil;
 import com.carcrafter.model.UserProfile;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Part;
@@ -15,10 +13,9 @@ import jakarta.servlet.http.Part;
 import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import jakarta.persistence.EntityManager;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
